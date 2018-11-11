66866061532e16a769a5377a9dc9f2fe689d5361
AutoStephen <43354043+AutoStephen@users.noreply.github.com>
2018-11-11 21:29:28 +0000

    Add files via upload

---
+++ SerialRev0/SerialRev0.ino
@@ -0,0 +1,9 @@
+void setup() {
+
+serial.begin(9600); //Set the communication speed and establish serial comm. 
+}
+
+void loop() {
+// Wait until we receive something from the computer.
+while(serial.available()==0) //Program will stay on this line until it receives something from the computer.
+}
