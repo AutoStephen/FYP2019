87dd8626fa33f052e53d6c6996ecce4552cca3b7
AutoStephen <43354043+AutoStephen@users.noreply.github.com>
2018-11-13 12:52:34 +0000

    Add files via upload
    Early Serial Programs

---
+++ SerialRev1.ino
@@ -0,0 +1,23 @@
+
+// 
+
+void setup() 
+{
+  Serial.begin(9600); //Set the communication speed and establish serial comm.
+}
+void loop() 
+{ 
+  String msg = "";
+
+  if (Serial.available()>0)
+  {
+    while(Serial.available()>0) // stay in this loop while there are bytes to be read in the serial buffer.
+    {
+      msg += char(Serial.read()); // adding characters to the string.
+      delay(25); // to ensure bits to not get lost.
+    }
+
+    Serial.println(msg);
+    
+  }
+}
