db02e9eb8d88331809660b8e6e6bb40a9f1a86fd
AutoStephen <43354043+AutoStephen@users.noreply.github.com>
2018-11-13 12:53:32 +0000

    Delete SerialRev0.ino

--- SerialRev0/SerialRev0.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-
-serial.begin(9600); //Set the communication speed and establish serial comm. 
-}
-
-void loop() {
-// Wait until we receive something from the computer.
-while(serial.available()==0) //Program will stay on this line until it receives something from the computer.
-}
